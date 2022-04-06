70ef68d6eb884ab058b712649a9aa6eeec819c30
KrotosBenjamin <kj.benjamin90@gmail.com>
2022-04-06 15:24:50 -0400

    updating script

--- src/cudaUtils.h
+++ src/cudaUtils.h
@@ -16,15 +16,15 @@
     }                                                \
   } while(0)
 
-#define CUDA_SAFE_CALL(call)
-do {
-    cudaError_t err = call;
-    if (cudaSuccess != err) {
-        fprintf(stderr, "Cuda error in file '%s' in line %i : %s.",
-                __FILE__, __LINE__, cudaGetErrorString(err));
-        exit(EXIT_FAILURE);
-    }
-  } while(0)
+#define CUDA_SAFE_CALL(call)                                      \
+  do {                                                            \
+    cudaError_t err = call;                                       \
+    if (cudaSuccess != err) {                                     \
+      fprintf(stderr, "Cuda error in file '%s' in line %i : %s.", \
+              __FILE__, __LINE__, cudaGetErrorString(err));       \
+      exit(EXIT_FAILURE);                                         \
+    }                                                             \
+  } while(0)                                                      \
 
 void cudaLaunch(std::string kernelName,
                 void * args[],
